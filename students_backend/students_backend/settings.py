2b8ded3dd42ca9af6c860c7d90bb8787d82286fd
givenng6 <givenng6@gmail.com>
2022-08-29 16:59:50 +0200

    Some

--- students_backend/students_backend/settings.py
+++ students_backend/students_backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uuedjjebs)+l%c+%)hql@k!7k!_^=fm1*046w428*k&^r7n^^)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'sdpbackend.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '10.0.1.55']
 
 
 # Application definition
--- students_backend/students_backend/urls.py
+++ students_backend/students_backend/urls.py
@@ -16,18 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from students_backend import state
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve
-from django.conf.urls import url
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('state/', state.state),
     path("auth/", include('authentication.urls')),
-
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-    
 ]
