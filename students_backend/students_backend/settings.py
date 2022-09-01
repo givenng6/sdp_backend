39c681219cd182f40dc368854dd7398c3fe159ac
givenng6 <givenng6@gmail.com>
2022-09-01 11:15:16 +0200

    Running

--- students_backend/students_backend/settings.py
+++ students_backend/students_backend/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-uuedjjebs)+l%c+%)hql@k!7k!_^=fm1*046w428*k&^r7n^^)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '10.0.1.55']
 
+ALLOWED_HOSTS = ['127.0.0.1', '10.0.1.55']
 
 # Application definition
 
