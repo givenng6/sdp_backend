47e6dd20b6b82c24e52e807643ed99919ef77d57
givenng6 <givenng6@gmail.com>
2022-09-05 17:04:17 +0200

    Try catch

--- students_backend/authentication/pyrebase.py
+++ students_backend/authentication/pyrebase.py
@@ -16,11 +16,14 @@ auth = app.auth()
 
 # send veryfication link...
 def sendLink(email, password):
-    user = auth.sign_in_with_email_and_password(email, password)
-    # before the 1 hour expiry:
-    user = auth.refresh(user['refreshToken'])
-    # now we have a fresh token
-    auth.send_email_verification(user['idToken'])
+    try:
+      user = auth.sign_in_with_email_and_password(email, password)
+      # before the 1 hour expiry:
+      user = auth.refresh(user['refreshToken'])
+      # now we have a fresh token
+      auth.send_email_verification(user['idToken'])
+    except:
+      pass
 
 def signIn(email, password):
   try:
