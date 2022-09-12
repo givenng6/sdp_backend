708478b082a1c6a47576dcd3525c1d4add68566f
givenng6 <givenng6@gmail.com>
2022-09-12 09:10:57 +0200

    render

--- students_backend/students_backend/settings.py
+++ students_backend/students_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +21,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uuedjjebs)+l%c+%)hql@k!7k!_^=fm1*046w428*k&^r7n^^)'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-uuedjjebs)+l%c+%)hql@k!7k!_^=fm1*046w428*k&^r7n^^)')
+#SECRET_KEY = 'django-insecure-uuedjjebs)+l%c+%)hql@k!7k!_^=fm1*046w428*k&^r7n^^)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 
 ALLOWED_HOSTS = ['127.0.0.1', '10.0.1.55', '192.168.137.217', '192.168.98.22', '10.203.238.89']
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:   
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
